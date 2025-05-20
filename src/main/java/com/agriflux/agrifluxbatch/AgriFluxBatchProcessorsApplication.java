622330d0d03b2ab3b70a045f170725751e487876
MettiuhGraziano <mettiuh96@gmail.com>
2025-05-20 19:00:39 +0200

    Primo Commit

---
+++ src/main/java/com/agriflux/agrifluxbatch/AgriFluxBatchProcessorsApplication.java
@@ -0,0 +1,13 @@
+package com.agriflux.agrifluxbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriFluxBatchProcessorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriFluxBatchProcessorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agriflux/agrifluxbatch/AgriFluxBatchProcessorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agriflux.agrifluxbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriFluxBatchProcessorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

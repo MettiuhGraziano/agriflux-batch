62bb01e6f70438f4f39aff552aea02bc168eed5d
MettiuhGraziano <mettiuh96@gmail.com>
2025-05-31 16:57:29 +0200

    Astratte le cache per miglioramento performance job

--- src/main/java/com/agriflux/agrifluxbatch/processor/DatiProcessor.java
+++ src/main/java/com/agriflux/agrifluxbatch/processor/DatiProcessor.java
@@ -1,13 +1,69 @@
 package com.agriflux.agrifluxbatch.processor;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
+import org.springframework.batch.core.annotation.BeforeStep;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+import com.agriflux.agrifluxbatch.entity.Coltura;
+import com.agriflux.agrifluxbatch.entity.Morfologia;
+import com.agriflux.agrifluxbatch.repository.ColturaRepository;
+import com.agriflux.agrifluxbatch.repository.DatiMorfologiciRepository;
+
+@Component
 public class DatiProcessor {
 	
+	@Autowired
+	private ColturaRepository colturaRepository;
+	
+	@Autowired
+	private DatiMorfologiciRepository datiMorfologiciRepository;
+	
+	private static final String ID_COLTURA = "idColtura";
+	private static final String ID_MORFOLOGIA = "idMorfologia";
 	private static final String DELIMITER = "|";
 	private static Random random = new Random();
 	
+	private static final Map<Long, Coltura> cacheColtura = new HashMap<Long, Coltura>();
+	private static final Map<Long, Morfologia> cacheMorfologia = new HashMap<Long, Morfologia>();
+	
+	@BeforeStep
+	public void init() {
+		
+		if (cacheColtura.isEmpty()) {
+			Iterable<Coltura> colture = colturaRepository.findAll(Sort.by(Sort.Direction.ASC, ID_COLTURA));
+
+			if (null != colture && colture.iterator().hasNext()) {
+				for (Coltura coltura : colture) {
+					cacheColtura.put(coltura.getIdColtura(), coltura);
+				}
+			}
+		}
+		
+		if (cacheMorfologia.isEmpty()) {
+			Iterable<Morfologia> morfologie = datiMorfologiciRepository.findAll(Sort.by(Sort.Direction.ASC, ID_MORFOLOGIA));
+
+			if (null != morfologie && morfologie.iterator().hasNext()) {
+				for (Morfologia morfologia : morfologie) {
+					cacheMorfologia.put(morfologia.getIdMorfologia(), morfologia);
+				}
+			}
+		}
+	}
+	
+	public Map<Long, Coltura> getCacheColtura() {
+		return cacheColtura;
+	}
+	
+	public static Map<Long, Morfologia> getCacheMorfologia() {
+		return cacheMorfologia;
+	}
+
 	protected static BigDecimal generaRandomBigDecimalFromRange(String range) {
 
 		int delimiterIndex = range.indexOf(DELIMITER);
--- src/main/java/com/agriflux/agrifluxbatch/processor/DatiProduzioneEnricherProcessor.java
+++ src/main/java/com/agriflux/agrifluxbatch/processor/DatiProduzioneEnricherProcessor.java
@@ -6,57 +6,20 @@ import java.util.Map;
 
 import org.springframework.batch.core.annotation.BeforeProcess;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 
 import com.agriflux.agrifluxbatch.entity.Coltura;
 import com.agriflux.agrifluxbatch.entity.Morfologia;
 import com.agriflux.agrifluxbatch.enumeratori.TipoProdottoEnum;
 import com.agriflux.agrifluxbatch.model.DatiProduzioneMetadata;
 import com.agriflux.agrifluxbatch.model.DatiProduzioneRecord;
-import com.agriflux.agrifluxbatch.repository.ColturaRepository;
-import com.agriflux.agrifluxbatch.repository.DatiMorfologiciRepository;
 
 public class DatiProduzioneEnricherProcessor extends DatiProcessor implements ItemProcessor<DatiProduzioneMetadata, DatiProduzioneRecord>{
 	
-	private static final String ID_MORFOLOGIA = "idMorfologia";
-	private static final String ID_COLTURA = "idColtura";
-
-	@Autowired
-	private ColturaRepository colturaRepository;
-	
-	@Autowired
-	private DatiMorfologiciRepository datiMorfologiciRepository;
-	
-	private final Map<Long, Coltura> cacheColtura = new HashMap<Long, Coltura>();
-	private final Map<Long, Morfologia> cacheMorfologia = new HashMap<Long, Morfologia>();
 	private final Map<String, Double> cacheVolumeProdotto = new HashMap<String, Double>();
 	private final Map<String, BigDecimal> cachePesoMedioProdotto = new HashMap<String, BigDecimal>();
 	
 	@BeforeProcess
 	public void beforeProcess() {
-		
-		if (cacheColtura.isEmpty()) {
-			
-			Iterable<Coltura> colture = colturaRepository.findAll(Sort.by(Sort.Direction.ASC, ID_COLTURA));
-			
-			if (null != colture && colture.iterator().hasNext()) {
-				for (Coltura coltura : colture) {
-					cacheColtura.put(coltura.getIdColtura(), coltura);
-				}
-			}
-		}
-		
-		if (cacheMorfologia.isEmpty()) {
-			
-			Iterable<Morfologia> morfologie = datiMorfologiciRepository.findAll(Sort.by(Sort.Direction.ASC, ID_MORFOLOGIA));
-			
-			if (null != morfologie && morfologie.iterator().hasNext()) {
-				for (Morfologia morfologia : morfologie) {
-					cacheMorfologia.put(morfologia.getIdMorfologia(), morfologia);
-				}
-			}
-		}
 		
 		if (cacheVolumeProdotto.isEmpty()) {
 			for (TipoProdottoEnum prodotto : TipoProdottoEnum.values()) {
@@ -100,8 +63,8 @@ public class DatiProduzioneEnricherProcessor extends DatiProcessor implements It
 		
 		BigDecimal fatturatoColtura = null;
 		
-		if (null != cacheColtura && !cacheColtura.isEmpty()) {
-			Coltura coltura = cacheColtura.get(fkIdColtura);
+		if (null != getCacheColtura() && !getCacheColtura().isEmpty()) {
+			Coltura coltura = getCacheColtura().get(fkIdColtura);
 			if (null != coltura) {
 				if (null != cachePesoMedioProdotto && !cachePesoMedioProdotto.isEmpty()) {
 					BigDecimal pesoProdotto = cachePesoMedioProdotto.get(coltura.getProdottoColtivato());
@@ -125,16 +88,14 @@ public class DatiProduzioneEnricherProcessor extends DatiProcessor implements It
 
 	private BigDecimal calcolaQuantitaRaccolto(long fkIdColtura, long fkIdMorfologia) {
 		
-		//Il calcolo si basa con la misurazione del terreno in metro quadro
-		
 		BigDecimal quantitaRaccolto = null;
 		
-		if (null != cacheMorfologia && !cacheMorfologia.isEmpty()) {
-			Morfologia morfologia = cacheMorfologia.get(fkIdMorfologia);
+		if (null != getCacheMorfologia() && !getCacheMorfologia().isEmpty()) {
+			Morfologia morfologia = getCacheMorfologia().get(fkIdMorfologia);
 			if (null != morfologia) {
 				BigDecimal estensioneTerreno = morfologia.getEstensioneTerreno();
-				if (null != cacheColtura && !cacheColtura.isEmpty()) {
-					Coltura coltura = cacheColtura.get(fkIdColtura);
+				if (null != getCacheColtura() && !getCacheColtura().isEmpty()) {
+					Coltura coltura = getCacheColtura().get(fkIdColtura);
 					if (null != coltura) {
 						String prodottoColtivato = coltura.getProdottoColtivato();
 						if (null != cacheVolumeProdotto && !cacheVolumeProdotto.isEmpty()) {
